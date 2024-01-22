41a13dd3d9c7375e41501e5ccfb0d8fe1df6ada1
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-01-22 08:10:01 +0000

    [Finish] 55-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/top'
   devise_for :users
   root to: "homes#top"
   
+  resources :books, only: [:new, :index, :show]do
+  resources :users, only: [:show, :edit]  
+end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
