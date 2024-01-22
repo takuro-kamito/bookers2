51ef84860802e155bec785fced417f7b1144316d
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-01-22 08:54:20 +0000

    [Finish] 55-18

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_22_064034) do
+ActiveRecord::Schema.define(version: 2024_01_22_085109) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2024_01_22_064034) do
     t.string "name"
     t.string "body"
     t.string "title"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
